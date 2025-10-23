af297ddbbc71b3642a14b632e69782ba14f80df1
Daniel Ornelas-Ochoa <danielornelasochoa@gmail.com>
2025-10-23 12:07:37 -0600

    added Clear Service

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -1,4 +1,16 @@
 package service;
 
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+
 public class ClearService {
+    private final DataAccess dataAccess;
+
+    public ClearService(DataAccess data) {
+        dataAccess = data;
+    }
+
+    public void clearApplication() throws DataAccessException {
+        dataAccess.clear();
+    }
 }
