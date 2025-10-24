82bc6558895623e31820134bd8955e38354cb05b
Daniel Ornelas-Ochoa <danielornelasochoa@gmail.com>
2025-10-23 23:27:41 -0600

    Implemented userService class

---
+++ server/src/main/java/service/Requests/LoginRequest.java
@@ -0,0 +1,3 @@
+package service.Requests;
+
+public record LoginRequest(String username, String password) {}
--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,4 +1,60 @@
 package service;
 
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import model.UserData;
+import model.AuthData;
+import service.Requests.LoginRequest;
+
+
 public class UserService {
+    private final DataAccess dataAccess;
+
+    public UserService(DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
+    }
+
+    public AuthData register(UserData user) throws DataAccessException {
+        if(user.username() == null || user.email() == null || user.password() == null){
+            throw new DataAccessException("Error: bad request.");
+        } if(dataAccess.getUser(user.username()) != null){
+            throw new DataAccessException("Error: already taken");
+        }
+
+        dataAccess.createUser(user);
+
+        return dataAccess.createAuth(user.username());
+    }
+
+    public AuthData login(LoginRequest request) throws DataAccessException {
+        if(request.username() == null || request.password() == null){
+            throw new DataAccessException("Error: bad request.");
+        }
+
+        UserData user = dataAccess.getUser(request.username());
+
+        if(user == null){
+            throw new DataAccessException("Error: unauthorized");
+        }
+
+        if(user.password() == null || !user.password().equals(request.password())){
+            throw new DataAccessException("Error: unauthorized");
+        }
+
+        return dataAccess.createAuth(user.username());
+    }
+
+    public void logout(String authToken) throws DataAccessException {
+        if(authToken == null){
+            throw new DataAccessException("Error: bad request.");
+        }
+        AuthData auth = dataAccess.getAuth(authToken);
+        if(auth == null){
+            throw new DataAccessException("Error: unauthorized");
+        }
+
+        dataAccess.deleteAuth(authToken);
+    }
+
+
 }
