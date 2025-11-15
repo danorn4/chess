c07103a6b94233248008217911e676ebd66fb326
Daniel Ornelas-Ochoa <danielornelasochoa@gmail.com>
2025-11-14 17:37:05 -0700

    check list

--- client/src/main/java/client/Repl.java
+++ client/src/main/java/client/Repl.java
@@ -2,8 +2,11 @@ package client;
 
 import exception.ResponseException;
 import model.AuthData;
+import model.GameData;
 import model.UserData;
+import service.servicehelpers.GameResult;
 
+import java.util.Collection;
 import java.util.Scanner;
 
 public class Repl {
@@ -45,18 +48,44 @@ public class Repl {
     public String eval(String input) {
         try {
             String[] args = input.split(" ");
-            String command = args[0].toLowerCase();
 
-            return switch (command) {
-                case "help" -> help();
-                case "quit" -> "quit";
-                case "login" -> loginHandler(args);
-                case "register" -> registerHandler(args);
-                default -> "Unknown command. Type 'help' for a list of available commands.";
-            };
+            // Pre-Login
+            if(!isLoggedIn) {
+                return preLoginEval(args);
+
+            // Post-Login
+            } else {
+                return postLoginEval(args);
+            }
+
+
         } catch (Exception e) {
             return "Error: " + e.getMessage();
         }
+    }
+
+    public String preLoginEval(String[] args) throws ResponseException {
+        String command = args[0].toLowerCase();
+        return switch (command) {
+            case "help" -> help();
+            case "quit" -> "quit";
+            case "login" -> loginHandler(args);
+            case "register" -> registerHandler(args);
+            default -> "Unknown command. Type 'help' for a list of available commands.";
+        };
+    }
+
+    public String postLoginEval(String[] args) throws ResponseException {
+        String command = args[0].toLowerCase();
+        return switch (command) {
+            case "help" -> help();
+            case "logout" -> logoutHandler(args);
+            case "create" -> createHandler(args);
+            case "list" -> listHandler(args);
+            case "join" -> "implement joinHandler()";
+            case "observe" -> "implement observeHandler()";
+            default -> "Unknown command. Type 'help' for a list of available commands.";
+        };
     }
 
     public String registerHandler(String[] args) throws ResponseException {
@@ -94,6 +123,71 @@ public class Repl {
         return "Logged in as " + username;
     }
 
+    public String logoutHandler(String[] args) throws ResponseException {
+        if(args.length != 1) {
+            return "Error: invalid command. Use: logout";
+        }
+
+        server.logout(authToken);
+
+        isLoggedIn = false;
+        authToken = null;
+
+        return "Successfully logged out";
+    }
+
+    public String createHandler(String[] args) throws ResponseException {
+        if(args.length != 2) {
+            return "Error: invalid command. Use: create <GAME_NAME>";
+        }
+
+        String gameName = args[1];
+
+        GameResult gameResult = server.createGame(authToken, gameName);
+
+        return "Successfully created game " + gameName;
+    }
+
+    public String listHandler(String[] args) throws ResponseException {
+        if(args.length != 1) {
+            return "Error: invalid command. Use: list";
+        }
+
+        Collection<GameData> listGames = server.listGames(authToken);
+
+        if (listGames == null || listGames.isEmpty()) {
+            return "No games available.";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int listCount = 1;
+
+        for (GameData gameData : listGames) {
+            String gameName = gameData.gameName();
+
+            // temp id
+            int gameID = gameData.gameID();
+
+            String whiteUsername = gameData.whiteUsername() != null ? gameData.whiteUsername() : "<empty>";
+            String blackUsername = gameData.blackUsername() != null ? gameData.blackUsername() : "<empty>";
+
+            String gameInfo = String.format("%d. <<<GameID: %d>>> %s (White: %s, Black: %s)%n",
+                    listCount++, gameID, gameName, whiteUsername, blackUsername);
+            sb.append(gameInfo);
+        }
+
+        return sb.toString();
+    }
+
+    /*
+    public String joinHandler(String[] args) throws ResponseException {
+        if(args.length != 3) {
+            return "Error: invalid command. Use: join <GAME_ID> [WHITE|BLACK]";
+        }
+
+        int gameID = ;
+    }
+    */
 
 
 
